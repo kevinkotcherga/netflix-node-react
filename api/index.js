14fae2bae4a8232d80c0e548e6ed12d81ea4ac5b
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-15 14:12:35 +0100

    ✨feat(routes): ajout de la route auth users

--- api/index.js
+++ api/index.js
@@ -2,17 +2,21 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    })
-    .then(()=> console.log('DB Connection Successful!'))
-    .catch((err) => console.log(err));
+	.connect(process.env.MONGO_URL)
+	.then(() => console.log('DB Connection Successfull'))
+	.catch(err => {
+		console.error(err);
+	});
 
-app.listen(8800, () => {
-  console.log('Backend server is running!');
+app.use(express.json());
+
+app.use('/api/auth', authRoute);
+
+app.listen(process.env.PORT || 8800, () => {
+	console.log('Backend server is running!');
 });
